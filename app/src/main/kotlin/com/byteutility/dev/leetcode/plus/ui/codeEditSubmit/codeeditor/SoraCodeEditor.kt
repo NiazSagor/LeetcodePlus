35b627a4e4894da1ac3cd66a57ddd49661a0e208
NiazSagor <niazsagor@gmail.com>
2026-01-11 19:36:53 +0600

    Remove unused `getTextFieldValue` extension function
    This commit removes the `getTextFieldValue` extension function for the `CodeEditor` as it is no longer used within the `SoraCodeEditor.kt` file.

--- app/src/main/kotlin/com/byteutility/dev/leetcode/plus/ui/codeEditSubmit/codeeditor/SoraCodeEditor.kt
+++ app/src/main/kotlin/com/byteutility/dev/leetcode/plus/ui/codeEditSubmit/codeeditor/SoraCodeEditor.kt
@@ -3,8 +3,6 @@ package com.byteutility.dev.leetcode.plus.ui.codeEditSubmit.codeeditor
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextRange
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.viewinterop.AndroidView
 import io.github.rosemoe.sora.widget.CodeEditor
 
@@ -37,23 +35,5 @@ fun SoraCodeEditor(
                 onCodeChange(code)
             }
         }
-    )
-}
-
-fun CodeEditor.getTextFieldValue(): TextFieldValue {
-    val editorText = this.text // This is of type Content
-
-    // In Sora, cursor.left and cursor.right are absolute character offsets
-    // If your version uses older cursor properties, use these:
-    val selectionStart = this.cursor.left
-    val selectionEnd = this.cursor.right
-
-    // Ensure we don't exceed the text length to avoid crashes
-    val safeStart = selectionStart.coerceIn(0, editorText.length)
-    val safeEnd = selectionEnd.coerceIn(0, editorText.length)
-
-    return TextFieldValue(
-        text = editorText.toString(),
-        selection = TextRange(safeStart, safeEnd)
     )
 }
